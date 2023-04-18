aa36a7c5418ad0f5bb2ba2852488758c847dd665
agpavlik <agpavlik@gmail.com>
2023-04-18 01:03:58 +0000

    EJS engine added

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//This tells the Express app to use EJS as its templating engine.
+app.set("view engine", "ejs")
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -16,11 +19,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
 // The response can contain HTML code, which would be rendered in the client browser.
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+//
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+ 
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 
 app.listen(PORT, () => {
