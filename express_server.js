da91ed04fd59e848b232d8512e3816587edd2058
agpavlik <agpavlik@gmail.com>
2023-04-18 03:39:57 +0000

    GET / POST added

--- express_server.js
+++ express_server.js
@@ -10,11 +10,26 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+//POST route to handle the form submission.
+// This needs to come before all of our routes.
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 // This is the data we'll want to show on the URLs page. Collect in folder 'views'.
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+// A GET route to render the urls_new.ejs template.
+app.get('/urls/new',(req, res) => {
+  res.render("urls_new");
+});
+
 
 // Add another page to display a single URL and its shortened form. The end point for 
 // a page will be in the format /urls/:id. The : in front of id indicates that id is 
@@ -35,17 +50,6 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-//
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
- 
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
