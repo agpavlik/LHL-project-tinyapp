5ebae78847880182ca79a3d566a499ae6cc10d85
agpavlik <agpavlik@gmail.com>
2023-04-19 01:33:32 +0000

    POST route to delete URL from urlDatabase

--- express_server.js
+++ express_server.js
@@ -10,10 +10,10 @@ const urlDatabase = {
 };
 
 /*The body-parser library will convert the request body from a Buffer
- into string that we can read. This needs to come before all of our routes. */
+ into string that we can read. This needs to come before all of routes. */
 app.use(express.urlencoded({ extended: true }));
 
-// Function generates a rendom short URL id by return a string of 6 random alphanumeric characters:
+// Function generates a random short URL id by return a string of 6 random alphanumeric characters:
 const generateRandomString = function() {
   const alphanum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let output = '';
@@ -24,24 +24,29 @@ const generateRandomString = function() {
 };
 
 
-//POST route to handle the form submission.
-// This needs to come before all of other routes.
+// POST route to handle the form submission.This needs to come before all of other routes.
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-   // Respond with 'Ok' (we will replace this)
   let shortURL = generateRandomString(); //add Short URL ID to the urlDatabase
   urlDatabase[shortURL] = req.body['longURL']; // add longURl to the urlDatabase
   res.redirect(`/u/${shortURL}`);
 });
 
+
+// POST route to delete URL from urlDatabase.
+app.post("/urls/:shortURL/delete", (req, res) => {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+});
+
+
 // MAIN PAGE
-// This is the data we want to show on the URLs page. Collect in 'urls_index'.
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-/* A GET route to render the urls_new.ejs template. 
+/* GET route to render the urls_new.ejs template. 
 The GET /urls/new route needs to be defined before the 
 GET /urls/:id route. Routes defined earlier will take precedence */
 app.get('/urls/new',(req, res) => {
@@ -49,16 +54,15 @@ app.get('/urls/new',(req, res) => {
 });
 
 
-/* This is a data for the page to display a single URL and its shortened form.
+/* GET route to display a single URL and its shortened form.
 The : in front of id indicates that id is a route parameter. 
 This means that the value in this part of the url will be available in the req.params object.*/
-
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL:'http://www.lighthouselabs.ca'};
   res.render("urls_show", templateVars);
 });
 
-// Redirection to long URL when given shortURL
+// GET route - Redirection to longURL when given shortURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
