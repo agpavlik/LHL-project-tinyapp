b12b4ec9d8eaf75c51980aeb7bdeaf7e56052e85
agpavlik <agpavlik@gmail.com>
2023-04-21 03:29:12 +0000

    Version 0.1 is finalised

---
+++ test/helpersTest.js
@@ -0,0 +1,36 @@
+const { assert } = require('chai');
+
+const { generateRandomString, getUserByEmail, getUserByPassword, getUrlByUserId } = require("../helpers");
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userId: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userId: "aJ49lW",
+  },
+};
+
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    assert.equal(user.id, expectedUserID)
+  });
+});
